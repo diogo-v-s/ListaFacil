5309aad4fa5b91892746a4a11f2c4c743cc2dbcf
Diogo Silva <diogo.dvs.white@gmail.com>
2026-09-23 07:27:45 -0300

    Add funcao de salvar a lista apos finalizar, e copiar listas antigas

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'lista-facil-v8';
+const CACHE = 'lista-facil-v9';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.svg', './icon-512.svg', './idb.js'];
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting()));
